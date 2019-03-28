4dab0b057a8293e2602d2cdce529bb7560ddec1d
p0lr <p0lr@p0lr.org>
2019-03-28 16:39:59 +0800

    Add files via upload

---
+++ dug.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+import xml.etree.ElementTree as ET
+import fw_creds
+import sqlite3
+import StringIO
+
+fwhost = fw_creds.fwhost
+fwkey = fw_creds.fwkey
+dbfile = "/home/pi/dug/devices.sql"
+xmlfile = StringIO.StringIO()
+rsafile = "/var/www/html/rsa.csv"
+
+def getDhcp(fwhost, fwkey):
+
+  values = {'type': 'op', 'cmd': '<show><dhcp><server><lease><interface>all</interface></lease></server></dhcp></show>', 'key': fwkey}
+  palocall = 'https://%s/api/' % (fwhost)
+  r = requests.post(palocall, data=values, verify=False)
+  leases = []
+  dhcptree = ET.fromstring(r.text)
+  for lease in dhcptree.findall('./result/interface/entry'):
+    info = {}
+    if (lease.find('state').text == "committed"):
+      info['ip'] = lease.find('ip').text
+      info['mac'] = lease.find('mac').text
+      if lease.find('hostname') is not None:
+        info['host'] = lease.find('hostname').text
+        info['sysname'] = "%s-%s" % (lease.find('mac').text, lease.find('hostname').text)
+      else:
+        info['sysname'] = lease.find('mac').text
+      leases.append(info)
+  return leases
+
+def getArp(fwhost, fwkey):
+  output = []
+  values = {'type': 'op', 'cmd': '<show><arp><entry name ="all"/></arp></show>', 'key': fwkey}
+  palocall = 'https://%s/api/' % (fwhost)
+  r = requests.post(palocall, data=values, verify=False)
+  arptree = ET.fromstring(r.text)
+  for entries in arptree.findall('./result/entries'):
+    for entry in entries.findall('entry'):
+      info = {}
+      info['ip'] = entry.find('ip').text
+      info['mac'] = entry.find('mac').text
+      info['sysname'] = entry.find('mac').text
+      output.append(info)
+  return output
+
+def getDatabase(dbfile):
+  conn = sqlite3.connect(dbfile)
+  cursor = conn.cursor()
+  cursor.execute('select DeviceName, DeviceMac, Groups from DevicesDynamic')
+  responses = cursor.fetchall()
+
+  output = []
+  for response in responses:
+    output.append( {"mac": response[1], "name": response[0], "group": response[2]} )
+
+  return output
+
+def enrichArp(leases):
+  arps = getArp(fwhost, fwkey)
+  for arp in arps:
+    addarp = True
+    for lease in leases:
+      if arp['mac'] == lease['mac']:
+        addarp = False
+
+    if addarp:
+      leases.append(arp)
+
+  return leases
+
+def enrichDatabase(leases):
+  databases = getDatabase(dbfile)
+  output = []
+  for lease in leases:
+    for record in databases:
+      if (lease['mac'] == record['mac']):
+        lease['sysname'] = record['name']
+        lease['group'] = record['group']
+    output.append(lease)
+  return output
+
+def fwFormat(hostdata):
+  fwxml = '<uid-message>\n'
+  fwxml += '\t<version>1.0</version>\n'
+  fwxml += '\t<type>update</type>\n'
+  fwxml += '\t<payload>\n'
+  fwxml += '\t\t<login>\n'
+  for entry in hostdata:
+    fwxml += '\t\t\t<entry name="%s" ip="%s" timeout="0"/>\n' % (entry['sysname'], entry['ip'])
+  fwxml += '\t\t</login>\n'
+  fwxml = fwxml + '\t\t<groups>\n'
+  groups = []
+  for entry in hostdata:
+    if entry.has_key('group'):
+      if entry['group'] not in groups:
+        groups.append(entry['group'])
+  for group in groups:
+    fwxml += '\t\t\t<entry name="%s">\n' % (group, )
+    fwxml += '\t\t\t\t<members>\n'
+    for entry in hostdata:
+      if entry.has_key('group'):
+        if (entry['group'] == group):
+          fwxml += '\t\t\t\t\t<entry name="%s"/>\n' % (entry['sysname'], )
+    fwxml += '\t\t\t\t</members>\n'
+    fwxml += '\t\t\t</entry>\n'
+  fwxml += '\t\t</groups>\n'
+  fwxml += '\t</payload>\n'
+  fwxml += '</uid-message>'
+  return fwxml
+
+def rsaWrite(hostdata):
+  rsacsv = 'mac,ip,sysname,host,group\n'
+  for entry in hostdata:
+    rsacsv += "%s,%s,%s" % (entry['mac'], entry['ip'], entry['sysname'])
+    if entry.has_key('host'):
+      rsacsv += ',%s' % (entry['host'], )
+    else:
+      rsacsv += ','
+    if entry.has_key('group'):
+      rsacsv += ',%s' % (entry['group'], )
+    else:
+      rsacsv += ','
+    rsacsv += '\n'
+  file = open(rsafile,"w+")
+  file.write(rsacsv)
+  file.close
+
+leases = getDhcp(fwhost, fwkey)
+arps = enrichArp(leases)
+hostdata = enrichDatabase(arps)
+
+rsaWrite(hostdata)
+
+fwxml = fwFormat(hostdata)
+
+xmlfile.write(fwxml)
+files = {xmlfile: xmlfile.getvalue()}
+values = {'type': 'user-id', 'key': fwkey}
+palocall = 'https://' + fwhost + '/api'
+r = requests.post(palocall, data=values, files=files, verify=False)
+rtree = ET.fromstring(r.text)
+print rtree.get('status')
