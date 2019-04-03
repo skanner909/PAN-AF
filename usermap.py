93d1e038868864a1fd5c0877b60e8b9ffcae61d4
p0lr <p0lr@p0lr.org>
2019-04-03 12:04:54 +0800

    Add files via upload

---
+++ usermap.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+import xml.etree.ElementTree as ET
+import fw_creds
+
+fwhost = fw_creds.fwhost
+fwkey = fw_creds.fwkey
+
+#Make call to firewall to get XML DHCP lease information
+values = {'type': 'op', 'cmd': '<show><user><ip-user-mapping><all></all></ip-user-mapping></user></show>', 'key': fwkey}
+palocall = 'https://%s/api/' % (fwhost)
+r = requests.post(palocall, data=values, verify=False)
+
+#Convert the response from the firewall to an ElementTree to parse as XML
+tree = ET.fromstring(r.text)
+
+if (tree.get('status') == "success"):
+  for entry in tree.findall('./result/entry'):
+    print entry.find('ip').text + ": " + entry.find('user').text
